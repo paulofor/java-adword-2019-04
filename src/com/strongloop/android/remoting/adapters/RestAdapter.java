0894f11c493de40271424432dc708061c7588b2f
tr626987 <tr626987@oi.corp.net>
2019-12-26 17:33:17 -0300

    dados

--- src/com/strongloop/android/remoting/adapters/RestAdapter.java
+++ src/com/strongloop/android/remoting/adapters/RestAdapter.java
@@ -416,8 +416,8 @@ public class RestAdapter extends Adapter {
             }
 
    
-            ProxyServer proxy = new ProxyServer("10.21.7.10",82, "tr626987", "Jenlop01");
-            //request.setProxyServer(proxy);
+            ProxyServer proxy = new ProxyServer("10.21.7.10",82, "tr626987", "Lafiti23");
+            request.setProxyServer(proxy);
             
             if ("GET".equalsIgnoreCase(method)) {
             	
